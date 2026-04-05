e3517b7852bb9f7b0ef808312baa4725160373ce
ILYUXA60 <123599358+ILYUXA60@users.noreply.github.com>
2026-04-05 18:53:45 +0300

    Fix Vite rollup resolution error for local fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
+import '@fontsource/inter'
 import './style.css'
 import App from './App'
 
